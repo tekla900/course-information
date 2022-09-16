eb2086258308d27d9eee0ce97cf5a5223c91d6cf
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-09-17 01:21:03 +0400

    anecdote step3

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -16,17 +16,27 @@ const App = () => {
     'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
   ];
-   
+
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0]);
 
   const generateRandom = () => {
     setSelected(Math.floor((Math.random() * anecdotes.length)));
   }
 
+  const vote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  }
+
+
   return (
     <div>
       <p>{anecdotes[selected]}</p>
-      <Button handleEvent={generateRandom} text='click me' />
+      <p>has {votes[selected]} votes</p>
+      <Button handleEvent={generateRandom} text='next anecdote' />
+      <Button handleEvent={vote} text='vote' />
     </div>
   )
 }
