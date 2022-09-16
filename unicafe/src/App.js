ebfe2ab88cf5a688e0918f03a6e16c74cc983880
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-09-17 00:43:51 +0400

    unicafe step3

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -14,13 +14,27 @@ const Stats = ({ name, quantity }) => (
   <p>{name} {quantity}</p>
 )
 
+const Statistics = ({ good, neutral, bad }) => {
+  const allRating = good + neutral + bad;
+
+  return (
+    <div>
+      <Stats name="good" quantity={good} />
+      <Stats name="neutral" quantity={neutral} />
+      <Stats name="bad" quantity={bad} />
+      <Stats name="all" quantity={allRating} />
+      <Stats name="average" quantity={(good - bad)/allRating} />
+      <Stats name="positive" quantity={good/allRating*100} />
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const allRating = good + neutral + bad;
 
   const handleGood = () => {
     setGood(good + 1);
@@ -42,12 +56,7 @@ const App = () => {
       <Button handleEvent={handleBad} text="bad" />
 
       <Header header={'statistics'} />
-      <Stats name="good" quantity={good} />
-      <Stats name="neutral" quantity={neutral} />
-      <Stats name="bad" quantity={bad} />
-      <Stats name="all" quantity={allRating} />
-      <p>average {(good - bad)/allRating}</p>
-      <p>positive {good/allRating*100}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
