934f8c9255c642448539b91d7130c0924087886f
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-09-20 02:15:05 +0400

    phonebook step6

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,10 +1,23 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note';
-import { useState } from 'react';
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data);
+      });
+  }, []);
+
 
   const addNote = (event) => {
     event.preventDefault()
