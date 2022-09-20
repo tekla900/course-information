52bfda11bd47654c86c3ca5cb80c0a1371f14d66
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-09-20 20:50:37 +0400

    data for countries step3

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,6 +1,35 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
+const api_key = process.env.REACT_APP_API_KEY
+
+const Weather = ({ city, api_key }) => {
+  const [newTemp, setNewTemp] = useState('');
+  const [newWInd, setNewWind] = useState('');
+  const [newImg, setnewImg] = useState('');
+
+  {
+    useEffect(() => {
+      axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
+        .then(response => {
+          setNewTemp(response.data.main.temp);
+          setnewImg(response.data.weather[0].icon);
+          setNewWind(response.data.wind.speed);          
+        });
+    }, [])
+  
+}
+return (
+  <div>
+    <h2>Weather in {city}</h2>
+    <p>temperature: {newTemp} Celsius</p>
+    <img src={`http://openweathermap.org/img/wn/${newImg}@2x.png`} />`
+    <p>wind: {newWInd} 1.34 m/s</p>
+  </div>
+)
+}
+
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [newSearch, setNewSearch] = useState('');
@@ -9,14 +38,24 @@ const App = () => {
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
-        // console.log(response.data);
         setCountries(response.data)
       });
   }, []);
 
+
+
   const handleSearch = (event) => {
     setNewSearch(event.target.value);
   };
+
+  const handleShow = (event) => {
+    const currentCountry = event.target.value;
+    setCountries(countries.filter(each => {
+      if (each.name.common.toLowerCase().includes(currentCountry.toLowerCase())) {
+        return each;
+      }
+    }))
+  }
 
   const countriesToShow = countries.filter(each => {
     if (each.name.common.toLowerCase().includes(newSearch.toLowerCase())) {
@@ -25,7 +64,6 @@ const App = () => {
   });
 
   if (countriesToShow.length > 10) {
-    console.log('app works 1');
     return (
     <div>
       <span>find countries: </span>
@@ -34,7 +72,6 @@ const App = () => {
     </div>
     )
   } else if (countriesToShow.length < 10 && countriesToShow.length > 1) {
-    console.log('app works 2');
     return (
       <div>
         <span>find countries: </span>
@@ -42,7 +79,12 @@ const App = () => {
         <ul>
           {countriesToShow.map(each => {
             return (
-              <li key={each.name.common}>{each.name.common} </li>
+              <li key={each.name.common}>
+                {each.name.common} 
+                <button onClick={handleShow} value={each.name.common}>
+                  show
+                </button>
+              </li>
             )
           })}
         </ul>
@@ -51,15 +93,12 @@ const App = () => {
   } else if (countriesToShow.length === 1) {
     const languageArr = Object.values(countriesToShow[0].languages);
     const capitalArr = Object.values(countriesToShow[0].capital);
-    console.log('app works 3');
-    console.log(languageArr);
     return (
       <div>
           <span>find countries: </span>
           <input value={newSearch} onChange={handleSearch} />
           <h2>{countriesToShow[0].name.common}</h2>
-          <p>Capital: {capitalArr.map(each => <span>{each}</span>)}</p>
-          {/* <p>Capital: {countriesToShow[0].capital}</p> */}
+          <p>Capital: {capitalArr.map(each => <span key={each}>{each}</span>)}</p>
           <p>Area: {countriesToShow[0].area}</p>
 
           <h4>languages: </h4>
@@ -73,11 +112,18 @@ const App = () => {
             }
           </ul>
         <img src={countriesToShow[0].flags.svg} />
+        <Weather city={capitalArr[0]} api_key={api_key} />
+       
       </div>
     )
   }
 
-  
+  return (
+    <div>
+      <span>find countries: </span>
+      <input value={newSearch} onChange={handleSearch} />
+    </div>
+  )
 }
 
 export default App;
