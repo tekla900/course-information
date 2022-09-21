07b4668a2ad9ba548eea9ecfd5c5e4423d31f141
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-09-21 18:21:55 +0400

    phonebook step7

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -33,10 +33,14 @@ const App = () => {
         number: newNumber,
         id: persons.length + 1
       };
-  
-      setPersons(persons.concat(personObject));
-      setNewName('');
-      setNewNumber('');
+
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data));
+          setNewName('');
+          setNewNumber('');
+        });
     }
   };
 
